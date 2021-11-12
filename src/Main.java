87df711f8301dfaf2187d291b4bcf5bb25bbe1f8
richard483 <richard.william483@gmail.com>
2021-11-12 13:15:39 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,134 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+		String string1 = "Qwertyuiop";
+		String string2 = "KitaBelajarJava";
+		int a = 2;
+		int b = 2;
+
+		
+//		//Str.length(); --> panjang dari string yang kita pilih
+//		int panjang = string1.length();
+//		System.out.println(panjang + string2.length());
+		
+//		//Str.startWith(); --> buat mencari kata dengan awalan yang dimau
+//		System.out.println(string1.startsWith("Qwe"));
+//		System.out.println(string2.startsWith("Kita"));
+		
+//		//Str.endsWith(); --> buat mencari kata dengan akhiran yang dimau
+//		System.out.println(string1.endsWith("iop"));
+//		System.out.println(string2.endsWith("java"));
+		
+//		//Str.equals(); --> buat mengecek apakah sebuah string memiliki nilai yang sama
+//		if(string1.equals(string2)) {
+//			System.out.println("String1 sama dengan string 2.");
+//		}else {
+//			System.out.println("String1 tidak sama dengan string 2.");
+//		}
+//		
+////		System.out.println(string1 == string2);
+////		System.out.println(string1.equals(string2)); --> Str.equals(); itu function dari tipe data String
+////		System.out.println(a == b); --> int tidak bisa menggunakan .equals() gara" int itu tipe data primitif
+		
+//		//Str.toUppperCase(); --> mengubah sebuah string menjadi huruh kapital
+//		string2 = "qwertyuiop";
+//		if(string1.toUpperCase().equals(string2.toUpperCase())) {
+//			System.out.println("Kata-katanya sama");
+//		}else {
+//			System.out.println("Kata-katanya beda");
+//		}
+		
+//		//Str.toLowerCase(); --> mengubah sebuah string ke huruf kecil
+//		System.out.println(string1.toLowerCase());
+		
+//		//Str.contains --> mencari apakah di sebuah string terdapat kata yang diacari
+//		String kalimat = "Pada hari minggu ku turut ayah ke kota";
+//		String diCari = "Minggu";
+//		
+//		System.out.println(kalimat.toUpperCase().contains(diCari.toUpperCase())); //function string manipulation dapat di kombinasikan
+		
+//		//Str.concat() --> menggabungkan string yang terdapat didalam kurung
+//		String hewan = "Ayam";
+//		String kataKerja = "Bertelur";
+//		String hasil = hewan.concat(" ").concat(kataKerja);
+//		System.out.println(hasil);
+		
+//		//Str.substring(); --> mengambil kata-kata string dalam index tertentu
+//		String kota = "Jakarta";
+//		System.out.println(kota.substring(2, 5));
+		
+		
+		//Math Function
+		
+//		
+//		int num1 = 2434;
+//		int num2 = 314;
+//		
+//		//Math.sqrt --> sqrt square root, yang berarti dia cari akar kuadrat dari angka yg di maksud
+//		int num3 = (int) Math.sqrt(num2);
+//		System.out.println(Math.sqrt(num2));
+//		System.out.println(num3);
+		
+		
+//		//Math.pow(); pow power, mencari hasil pangkat bilanagan
+//		int num1 = 45;
+//		double num2 = Math.pow(num1, 2);
+//		System.out.println(num2);
+		
+//		//Math.ceil(); --> membulatkan bilangan ke atas
+//		double num1 = 12.00000000001;
+//		System.out.println(Math.ceil(num1));
+		
+		
+//		//Math.floor(); --> membulatkan bilanagn ke bawah
+//		double num1 = 12.9999;
+//		double num2 = Math.floor(num1);
+//		System.out.println(num1);
+//		System.out.println(num2);
+		
+//		//Math.rounf(); --> ditengah" ceil dan floor, membulatkan sewajarnya
+//		double num1 = 1.6777;
+//		double num2 = 1.499;
+//		
+//		System.out.println(Math.round(num1));
+//		System.out.println(Math.round(num2));
+		
+//		//operasi trigonometri
+//		double num1 = Math.sin(Math.toRadians(90)); //parameter yang diminta di Math.sin berupa sudut dalam radian 
+//		System.out.println(num1);
+//		
+//		double num2 = Math.toRadians(180);
+//		System.out.println(num2);
+//		System.out.println(Math.cos(num2));
+//		
+//		System.out.println(Math.tan(Math.toRadians(60)));
+//		
+		
+//		//Math.random(); memberikan angka random diantara 0.0 - 0.99999999
+//		double num1 = Math.random();
+//		System.out.println(num1);
+//		int num2 = (int)(Math.random() * 10);
+//		System.out.println(num2);
+//		
+//		int b1 = (5) + (int)(Math.random() * (10 - 5 + 1));
+//		System.out.println(b1);
+		
+		
+		
+		
+		//Array
+		int variabel1 = 0;
+		int[] variabelArray = {134, 222, 145, 90, 100};
+		System.out.println(variabel1);
+		System.out.println(variabelArray[1]);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Main();
+	}
+
+}
